095bbf54c465d73701503fb7be9a7f23bab03e04
Humbert Meyers <humbertmeyers@gmail.com>
2020-07-19 00:47:53 +0200

    création IPTest

---
+++ src/testsUnitaires/IPTest.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package testsUnitaires;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Humbert Meyers
+ *
+ */
+class IPTest {
+
+	/**
+	 * Test method for {@link model.IP#IP()}.
+	 */
+	@Test
+	void testIP() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link model.IP#IP(java.lang.String)}.
+	 */
+	@Test
+	void testIPString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link model.IP#IP(java.lang.String, int)}.
+	 */
+	@Test
+	void testIPStringInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link model.IP#getIpAdr()}.
+	 */
+	@Test
+	void testGetIpAdr() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link model.IP#getMasque()}.
+	 */
+	@Test
+	void testGetMasque() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link model.IP#toString()}.
+	 */
+	@Test
+	void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ src/testsUnitaires/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Ce package contient les classes servant aux tests unitaires de l'application
+ * @author Humbert Meyers
+ *
+ */
+package testsUnitaires;
