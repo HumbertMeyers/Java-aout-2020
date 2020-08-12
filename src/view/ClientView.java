f5985777c5f941b150f1b29ef9bf08e0cf1fbd37
Humbert Meyers <humbertmeyers@gmail.com>
2020-08-12 19:57:39 +0200

    Debut Observable
    ajout des fonctions importantes (interfaçage)

--- src/view/ClientView.java
+++ src/view/ClientView.java
@@ -19,6 +19,16 @@ public class ClientView implements Observer{
 	public ClientView() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void addObserver(Observer obs) {
+		
+	}
+	public void removeObserver() {
+	
+	}
+	public void notifyObserver(String str) {
+	
+	}
 
 	@Override
 	public void update(Observable o, Object arg) {
--- src/view/ServerView.java
+++ src/view/ServerView.java
@@ -19,6 +19,16 @@ public class ServerView implements Observer{
 	public ServerView() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void addObserver(Observer obs) {
+	
+	}
+	public void removeObserver() {
+	
+	}
+	public void notifyObserver(String str) {
+	
+	}
 
 	/**
 	 * @param args
