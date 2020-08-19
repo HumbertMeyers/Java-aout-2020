b76779418b783e5dbe3ae86e774daf0bf7df6be8
Humbert Meyers <humbertmeyers@gmail.com>
2020-08-19 03:13:18 +0200

    Application de tests sur la classe IP
    coverage : 93%

--- src/testsUnitaires/IPTest.java
+++ src/testsUnitaires/IPTest.java
@@ -6,43 +6,32 @@ package testsUnitaires;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import model.IP;
 
 /**
  * @author Humbert Meyers
  *
  */
 class IPTest {
-
-	/**
-	 * Test method for {@link model.IP#IP()}.
-	 */
-	@Test
-	void testIP() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link model.IP#IP(java.lang.String)}.
-	 */
-	@Test
-	void testIPString() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link model.IP#IP(java.lang.String, int)}.
-	 */
-	@Test
-	void testIPStringInt() {
-		fail("Not yet implemented"); // TODO
-	}
+	
+	IP test1 = new IP(24);
+	IP test2 = new IP();
+	IP test3 = new IP("192.168.1.5");
+	IP test4 = new IP("192.168.1.1", 24);
+	IP test5 = new IP("192.168.1.2", 28);
+	IP test6 = new IP("192.168.1.1", 24);
+	IP test7 = new IP("192.168.1.1");
+	IP test8 = new IP();
+	
 
 	/**
 	 * Test method for {@link model.IP#getIpAdr()}.
 	 */
 	@Test
 	void testGetIpAdr() {
-		fail("Not yet implemented"); // TODO
+		assertNotEquals(test1, test3);
+		test1.getIpAdr().equals(test4.getIpAdr());
+		test1.getIpAdr().equals(test5.getIpAdr());
 	}
 
 	/**
@@ -50,15 +39,18 @@ class IPTest {
 	 */
 	@Test
 	void testGetMasque() {
-		fail("Not yet implemented"); // TODO
+		assertNotEquals(test1.getMasque(), "255.255.252.0");
+		assertEquals(test2.getMasque(), "255.255.0.0");
 	}
 
 	/**
-	 * Test method for {@link model.IP#toString()}.
+	 * Test method for {@link model.IP#getMasqueInt(java.lang.String)}.
 	 */
 	@Test
-	void testToString() {
-		fail("Not yet implemented"); // TODO
+	void testGetMasqueInt() {
+		assertEquals(test2.getMasqueInt("255.255.0.0"), 16);
+		assertEquals(test7.getMasqueInt("255.255.255.252"), 30);
+		assertEquals(test4.getMasqueInt(test4.getMasque()), 24);
 	}
 
 }
